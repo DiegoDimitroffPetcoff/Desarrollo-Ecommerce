e29b2edfabe761141dd9b50306e8bb7f989b2c6f
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2022-08-26 05:49:56 -0300

    organizacion test

---
+++ test.manual.js
@@ -0,0 +1,16 @@
+// --------------------------------------|
+// ----------------TEST------------------|
+// --------------------------------------|
+const Todos = require('./test/todos');
+
+const todos = new Todos()
+console.log(todos.list());
+
+todos.add("run code")
+console.log(todos.list());
+
+todos.add("otra tarea")
+console.log(todos.list());
+
+todos.complete("run code")
+console.log(todos.list());
